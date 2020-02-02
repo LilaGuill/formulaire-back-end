1d18546bdbdcb47fd390ba9cd9a477c25a9bf2e6
Lila Guillermic <lila.guillermic@gmail.com>
2020-02-02 15:02:23 +0100

    cors

--- index.js
+++ index.js
@@ -8,15 +8,6 @@ require("dotenv").config();
 app.use(middlewareFormidable());
 app.use(cors());
 
-app.use(function(req, res, next) {
-  res.header(
-    "Access-Control-Allow-Headers",
-    "https://keen-archimedes-55635e.netlify.com/",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
-
 app.post("/send", (req, res) => {
   try {
     const DOMAIN = process.env.DOMAIN;
