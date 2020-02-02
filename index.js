5ca09a19728471b1f47bc7f675df6c45bcf612a6
Lila Guillermic <lila.guillermic@gmail.com>
2020-02-02 09:17:25 +0100

    route

--- index.js
+++ index.js
@@ -1,12 +1,38 @@
 const express = require("express");
-const cors = require("cors");
 const middlewareFormidable = require("express-formidable");
+const mailgun = require("mailgun-js");
+const cors = require("cors");
 const app = express();
 app.use(middlewareFormidable());
 app.use(cors());
 
-const mailGun = require("./routes/mailgun");
-app.use(mailGun);
+require("dotenv").config();
+
+app.post("/send", (req, res) => {
+  try {
+    const DOMAIN = process.env.DOMAIN;
+    const mg = mailgun({
+      apiKey: process.env.APIKEY,
+      domain: DOMAIN
+    });
+    const data = {
+      from: "Mailgun Sandbox <postmaster@" + DOMAIN + ">",
+      to: "lila.guillermic@gmail.com",
+      subject: "MailGun - Nouveau message",
+      text: `subject: ${req.fields.subject}
+       from: ${req.fields.name}
+       message: ${req.fields.message}
+       email: ${req.fields.email} `
+    };
+
+    mg.messages().send(data, function(error, body) {
+      // console.log(body);
+    });
+    res.send("message envoyé");
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
 
 app.listen(process.env.PORT, (req, res) => {
   console.log("server started");
